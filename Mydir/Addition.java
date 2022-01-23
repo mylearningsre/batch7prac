1b698adfef3b4154f4c99669cc4c465e197cee74
Fayaz <fana.fayaz@gmail.com>
2022-01-23 08:19:35 +0530

    Added new file Addition.java and modified Sample.java

---
+++ Mydir/Addition.java
@@ -0,0 +1,11 @@
+clas Addition
+{
+public static void main(String args[])
+{
+	int a,b,res;
+	a=10;
+	b=25;
+	res=a+b;
+	System.out.println("The sum is "+res);
+}
+}
--- Sample.java
+++ Sample.java
@@ -4,5 +4,6 @@ class Sample
 public static void main(String args[])
 {
 System.out.println("Welcome Batch 7");
+System.out.println("Thank you for joining devops course");
 }
 }
