6fb7600df36a9cb4931c3899f4fd07e3321d0c53
PMFayazAhmed <91198298+PMFayazAhmed@users.noreply.github.com>
2022-01-23 10:50:14 +0530

    Added one comment
    Comment is to understand what happens in program

--- Sample.java
+++ Sample.java
@@ -4,6 +4,7 @@ class Sample
 public static void main(String args[])
 {
 System.out.println("Welcome Batch 7 Devops Team");
+  //This line is a comment
 System.out.println("Thank you for joining devops course");
 }
 }
