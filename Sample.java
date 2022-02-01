3d53a53281ec0191ee8b751fd080fdf8a464a089
Fayaz <fana.fayaz@gmail.com>
2022-02-02 04:31:04 +0530

    Revert "Revert "Added one new line in Sample.java""
    This reverts commit ea768318e41f23680c7da69c9833a76fa855adb8.

--- Sample.java
+++ Sample.java
@@ -5,6 +5,7 @@ public static void main(String args[])
 {
 System.out.println("Welcome Batch 7 Devops Team");
   //This line is a comment
+System.out.println("Unwanted line");
 System.out.println("Thank you for joining devops course");
 System.out.println("Added extra line");
 }
