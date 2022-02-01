df5434d37fb1b08f89be80664a989cd9a2ae4a55
Fayaz <fana.fayaz@gmail.com>
2022-02-02 04:52:52 +0530

    One line added

--- Sample.java
+++ Sample.java
@@ -3,6 +3,7 @@ class Sample
 {
 public static void main(String args[])
 {
+	System.out.println("This is added in master branch");
 System.out.println("Welcome Batch 7 Devops Team");
 System.out.println("Unwanted line");
 System.out.println("Thank you for joining devops course");
