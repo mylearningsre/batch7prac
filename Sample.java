de4e7d1914f102637f4c9318302badccd5861896
Fayaz <fana.fayaz@gmail.com>
2022-02-02 04:42:54 +0530

    Added notime

--- Sample.java
+++ Sample.java
@@ -8,5 +8,6 @@ System.out.println("Welcome Batch 7 Devops Team");
 System.out.println("Unwanted line");
 System.out.println("Thank you for joining devops course");
 System.out.println("Added extra line");
+System.out.println("Date & Time not used : ");
 }
 }
