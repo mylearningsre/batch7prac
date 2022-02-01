5b537d7a37f4d674d77cc0f0d6a0f118e7d9aad5
Fayaz <fana.fayaz@gmail.com>
2022-02-02 04:54:17 +0530

    Thank you

--- Sample.java
+++ Sample.java
@@ -7,5 +7,6 @@ System.out.println("Welcome Batch 7 Devops Team");
 System.out.println("Unwanted line");
 System.out.println("Thank you for joining devops course");
 System.out.println("Added extra line");
+System.out.println("This is from fayaz branch");
 }
 }
