50ea3c90236c6bbe3e6e32a146c2af49836213c8
Fayaz <fana.fayaz@gmail.com>
2022-02-02 18:36:53 +0530

    One more comment line

--- Sample.java
+++ Sample.java
@@ -8,5 +8,6 @@ System.out.println("Welcome Batch 7 Devops Team");
 System.out.println("Thank you for joining devops course");
 System.out.println("Added extra line");
 //This is a comment at the end of main()
+//One more comment line
 }
 }
