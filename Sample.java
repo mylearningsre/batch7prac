24974e29d61be1e210a1659b9c4fd75180cea9f7
Fayaz <fana.fayaz@gmail.com>
2022-02-02 18:35:56 +0530

    Added one comment at the end of main

--- Sample.java
+++ Sample.java
@@ -7,5 +7,6 @@ System.out.println("Welcome Batch 7 Devops Team");
   //This line is a comment
 System.out.println("Thank you for joining devops course");
 System.out.println("Added extra line");
+//This is a comment at the end of main()
 }
 }
