faaf77c1cf65f0143de3f98243feb8e2be1d06cb
mylearningsre <mylearningsre@gmail.com>
2022-09-27 22:05:37 +0530

    modified sop statements

--- Sample.java
+++ Sample.java
@@ -5,8 +5,8 @@ class Sample
 public static void main(String args[])
 {
 	System.out.println("This is added in master branch");
-System.out.println("Welcome Batch 11 Devops Team");
-System.out.println("Good Morning");
+System.out.println("Welcome Batch 11 GCP Devops Team");
+System.out.println("Good Evening");
 System.out.println("Thank you for joining devops course");
 System.out.println("Added extra line");
 System.out.println("Date & Time not used : ");
