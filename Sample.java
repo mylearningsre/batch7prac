a0f7cb333dfb78b5e273678672c6e0be997b5291
Fayaz <fana.fayaz@gmail.com>
2022-09-27 07:24:56 +0530

    Modified SOP statements

--- Sample.java
+++ Sample.java
@@ -5,8 +5,8 @@ class Sample
 public static void main(String args[])
 {
 	System.out.println("This is added in master branch");
-System.out.println("Welcome Batch 7 Devops Team");
-System.out.println("Unwanted line");
+System.out.println("Welcome Batch 11 Devops Team");
+System.out.println("Good Morning");
 System.out.println("Thank you for joining devops course");
 System.out.println("Added extra line");
 System.out.println("Date & Time not used : ");
